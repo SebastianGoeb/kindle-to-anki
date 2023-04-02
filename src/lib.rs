2d575b4cc7830c91cae52e8de1a62abfda73dfc5
Sebastian Göb <sebastiancgoeb@gmail.com>
2023-04-02 14:41:26 +0200

    implement and test get_words

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,52 @@
 use std::env;
 
-use futures::TryStreamExt;
 use sqlx::{Connection, SqliteConnection};
 
-#[derive(sqlx::FromRow, Debug)]
-struct Word {
-    id: String,
-    word: String,
-    stem: String,
-    lang: String,
-    category: i32,
-    timestamp: i32,
-    profileid: String,
+#[derive(sqlx::FromRow, PartialEq, Debug)]
+pub struct Word {
+    pub id: String,
+    pub word: String,
+    pub stem: String,
+    pub lang: String,
+    pub category: i32,
+    pub timestamp: i32,
+    pub profileid: String,
 }
 
-pub async fn log_words() -> Result<(), sqlx::Error> {
+pub async fn get_words() -> Result<Vec<Word>, sqlx::Error> {
     let cwd = env::current_dir()
         .unwrap()
         .to_str()
         .expect("parseable cwd")
         .to_string();
-    let mut conn =
-        SqliteConnection::connect(&format!("file:///{}/tmp/vocab.sqlite", cwd)[..]).await?;
-    // let mut conn = SqliteConnection::connect("sqlite::memory:").await?;
-    let mut rows = sqlx::query_as::<_, Word>("SELECT * FROM WORDS").fetch(&mut conn);
-
-    while let Some(row) = rows.try_next().await? {
-        println!("{:?}", &row);
-    }
-
-    Ok(())
+    let path: String = format!("file:///{}/tmp/vocab.sqlite", cwd);
+    let mut conn = SqliteConnection::connect(&path).await?;
+    sqlx::query_as::<_, Word>("SELECT * FROM WORDS")
+        .fetch_all(&mut conn)
+        .await
 }
-
-// async fn select_words() -> sqlx::query::QueryAs<i64, Word, i64> {
-//     sqlx::query_as::<_, Word>("SELECT * FROM WORDS")
-// }
 
 #[cfg(test)]
 mod tests {
-    use std::env;
 
-    use sqlx::{Connection, SqliteConnection};
-
-    use crate::{log_words, Word};
+    use crate::{get_words, Word};
 
     #[async_std::test]
-    async fn should_get_many_rows() -> Result<(), sqlx::Error> {
-        log_words().await?;
-        Ok(())
-    }
-
-    #[async_std::test]
-    #[ignore]
-    async fn should_get_single_row() -> Result<(), sqlx::Error> {
-        let cwd = env::current_dir()
-            .unwrap()
-            .to_str()
-            .expect("parseable cwd")
-            .to_string();
-        let mut conn =
-            SqliteConnection::connect(&format!("file:///{}/tmp/vocab.sqlite", cwd)[..]).await?;
-        let row = sqlx::query_as::<_, Word>("SELECT * FROM WORDS")
-            .fetch_all(&mut conn)
-            .await?;
-        println!("{:?}", dbg!(row));
-        // let actual = select_words();
-        // assert_eq!(1, 2);
+    async fn should_get_words() -> Result<(), sqlx::Error> {
+        let result = get_words().await?;
+        assert_eq!(result.len(), 121);
+        assert_eq!(
+            result[0],
+            Word {
+                id: String::from("de:verwendet"),
+                word: String::from("verwendet"),
+                stem: String::from("verwenden"),
+                lang: String::from("de"),
+                category: 0,
+                timestamp: -1253962635,
+                profileid: String::from(""),
+            }
+        );
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use kindle_to_anki::get_words;
+
+#[async_std::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let words = get_words().await?;
+    for word in words {
+        println!("{:?}", word);
+    }
+    Ok(())
 }
